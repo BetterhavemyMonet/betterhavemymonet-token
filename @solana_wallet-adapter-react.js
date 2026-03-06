58723c2050d8e926a388da4dedd1fa08a13d699f
BetterhavemyMonet <xomonetox168@outlook.com>
2026-03-06 00:34:53 +0000

    Auto Web3 deploy + flatten directories

---
+++ @solana_wallet-adapter-react.js
@@ -0,0 +1,34 @@
+import {
+  ConnectionContext,
+  ConnectionProvider,
+  WalletContext,
+  WalletNotSelectedError,
+  WalletProvider,
+  useAnchorWallet,
+  useConnection,
+  useLocalStorage,
+  useWallet
+} from "./chunk-KO6DLDA7.js";
+import "./chunk-7NVM27MO.js";
+import "./chunk-ZBWFZWPY.js";
+import "./chunk-CBHQCOBP.js";
+import "./chunk-ALGTSMD3.js";
+import "./chunk-42XXHGZT.js";
+import "./chunk-ARPCE7O5.js";
+import "./chunk-3MNTPXTM.js";
+import "./chunk-GOCGMA7Z.js";
+import "./chunk-PWBYASA3.js";
+import "./chunk-WGW2LFOE.js";
+import "./chunk-UJVYIYX2.js";
+import "./chunk-RZ55PUNP.js";
+export {
+  ConnectionContext,
+  ConnectionProvider,
+  WalletContext,
+  WalletNotSelectedError,
+  WalletProvider,
+  useAnchorWallet,
+  useConnection,
+  useLocalStorage,
+  useWallet
+};
